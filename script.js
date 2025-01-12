9b49147fb0fab64bb1bf28d4b1022b1272844ef9
fulgenceBeugre <fulgencebeugre49@gmail.com>
2025-01-12 22:57:45 +0000

    Ajout du javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+let btnAdd = document.getElementById("add");
+
+btnAdd.addEventListener("click", function () {
+  prenom = "Fulgence";
+  alert(`Je m'appelle ${prenom}, je viens d'ajouter un produit`);
+});
